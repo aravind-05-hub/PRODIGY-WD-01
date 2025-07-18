56451362ffd62dee4621d98f2fe58916da84224a
aravind-05-hub <kannansaravind3@gmail.com>
2025-07-18 21:21:48 +0530

    Update cv.py

--- cv.py
+++ cv.py
@@ -1,1 +1,26 @@
-code
+and perform action
+        if length < 50 and delayCounter == 0:
+            for i, button in enumerate(buttonList):
+                if button.checkClick(x, y):
+                    myValue = buttonListValues[int(i % 4)][int(i / 4)]  # get correct number
+                    if myValue == '=':
+                        myEquation = str(eval(myEquation))
+                    else:
+                        myEquation += myValue
+                    delayCounter = 1
+
+    # to avoid multiple clicks
+    if delayCounter != 0:
+        delayCounter += 1
+        if delayCounter > 10:
+            delayCounter = 0
+
+    # Write the Final answer
+    cv2.putText(img, myEquation, (810, 130), cv2.FONT_HERSHEY_PLAIN,
+                3, (0, 0, 0), 3)
+
+    # Display
+    key = cv2.waitKey(1)
+    cv2.imshow("Image", img)
+    if key == ord('c'):
+        myEquation = ''
